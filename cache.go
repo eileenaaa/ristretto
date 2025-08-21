2eae80a4fec2c9eb1d5a89460e80f689d4ca8c45
eileenaaa <eileenaaa@163.com>
2025-08-21 16:37:20 +0800

    modify buf size

--- cache.go
+++ cache.go
@@ -33,7 +33,7 @@ import (
 
 var (
 	// TODO: find the optimal value for this or make it configurable
-	setBufSize = 256
+	setBufSize = 2
 )
 
 const itemSize = int64(unsafe.Sizeof(storeItem[any]{}))
